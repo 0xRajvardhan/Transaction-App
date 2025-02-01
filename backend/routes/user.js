59e95fdc19383158d76c8aa232495bb09c7225d9
CarlGustav <singhrajvardhan2003@gmail.com>
2025-02-01 19:02:01 +0530

    route to update user info

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,6 +4,7 @@ const User = require('../config/db');
 const jwt = require('jsonwebtoken');
 const JWT_SECRET = require('../config');
 const router = express.Router(); //initializing express router
+const { authMiddleware } = require('../middleware');
 
 // Signup Schema Validation using Zod
 const signupSchema = z.object({
@@ -12,22 +13,33 @@ const signupSchema = z.object({
     firstName: z.string().trim().min(3).max(20),
     lastName: z.string().trim().min(3).max(20),
 })
+
 // Signin Schema Validation using Zod
 const signinSchema = Zod.object({
     username: Zod.string().trim().lowercase().min(4).max(20),
     password: Zod.string(),
 })
 
+// Update Body Validation using Zod
+const updateBody = Zod.object({
+    password: Zod.string().optional(),
+    firstName: Zod.string().optional(),
+    lastName: Zod.string().optional(),
+});
+
 // Signup Route and Logic
 router.post("/signup", async (req, res) => {
-    const { success } = signupSchema.safeParse(req.body);
+    const { success } = signupSchema.safeParse(req.body); //validating the request body using zod
     if (!success) {
         return res.status(411).json({ message: 'Invalid inputs' });
     }
+
     // checking if the user already exists
     const existingUser = await User.findOne({
         username: req.body.username
     })
+
+    // If the user already exists, return an error
     if (existingUser._id) {
         return res.status(411).json({ message: 'Username already exists' });
     }
@@ -47,7 +59,7 @@ router.post("/signup", async (req, res) => {
 
 // Signin Route and Logic
 router.post("/signin", async (req, res) => {
-    const { success } = signinSchema.safeParse(req.body);
+    const { success } = signinSchema.safeParse(req.body); //validating the request body using zod
     if (!success) {
         res.status(411).json({ message: 'invalid inputs' })
     }
@@ -71,10 +83,27 @@ router.post("/signin", async (req, res) => {
         return;
     }
 
+    // If the user does not exist, return an error
     res.status(411).json({
         message: "Error while logging in"
     })
 
 });
+
+// Update Route and Logic
+router.put("/", authMiddleware, async (req, res) => {
+    const { success } = updateBody.safeParse(req.body); //validating the request body using zod
+    if (!success) {
+        res.status(411).json({
+            message: "Error while updating information"
+        })
+    }
+
+    await User.updateOne({ _id: req.userId }, req.body) //updating the user information
+
+    res.json({
+        message: "Updated Successfully"
+    })
+})
 
 module.exports = router;
