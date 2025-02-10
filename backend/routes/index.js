6a8d6f3ffbdf6e752b73326ffaab146dded8d1ec
CarlGustav <singhrajvardhan2003@gmail.com>
2025-02-10 15:24:13 +0530

    added accounts router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const userRouter = require('./user');
+const accountRouter = require("./account");
 
 const router = express.Router(); //initializing express router
 
 router.use('/user', userRouter); //using user router for /user path
+router.use('/account', accountRouter); //using account router for /account path
 
 module.exports = router;
